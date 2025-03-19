735260fe12461bd72b2d217be1c070b2c29e583e
Hamza Baig <hamza.baig@mergestack.com>
2025-03-19 16:15:57 +0500

    * add mobile menu * update filter dropdown styles

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import logo from "@assets/images/logo.png";
 import "./index.css";
 
@@ -7,35 +8,54 @@ interface FooterProps {
 }
 
 const Navbar: React.FC<FooterProps> = ({ resetFilters }) => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        {/* Logo */}
-        <div className="navbar-left">
-          <img src={logo} alt="Company Logo" className="navbar-logo" />
-
-          {/* Slogan */}
-          <h1 className="navbar-title">
-            Éclairer vos projets, bâtir votre avenir !
-          </h1>
-        </div>
-        <div className="navbar-right">
-          <div className="navbar-links">
-            <a href="#" target="_blank" rel="noopener noreferrer" className="navbar-item">
-              Contact
-            </a>
-
-            <a href="#" target="_blank" rel="noopener noreferrer" className="navbar-item">
-              About
-            </a>
-
-            <div onClick={resetFilters} className="navbar-item navbar-button">
-              Reset Filters
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <div className="navbar-left">
+            <img src={logo} alt="Company Logo" className="navbar-logo" />
+            <h1 className="navbar-title">
+              Éclairer vos projets, bâtir votre avenir !
+            </h1>
+          </div>
+          <div className="navbar-right">
+            <div className="navbar-links">
+              <a href="#" target="_blank" rel="noopener noreferrer" className="navbar-item">
+                Contact
+              </a>
+              <a href="#" target="_blank" rel="noopener noreferrer" className="navbar-item">
+                About
+              </a>
+              <div onClick={resetFilters} className="navbar-item navbar-button">
+                Reset Filters
+              </div>
+            </div>
+            <div className="mobile-menu-icon" onClick={toggleMobileMenu}>
+              {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
             </div>
           </div>
         </div>
+
+      </nav>
+      <div className={`mobile-menu ${isMobileMenuOpen ? "open" : ""}`}>
+        <a href="#" target="_blank" rel="noopener noreferrer" className="mobile-menu-item">
+          Contact
+        </a>
+        <a href="#" target="_blank" rel="noopener noreferrer" className="mobile-menu-item">
+          About
+        </a>
+        <div onClick={resetFilters} className="mobile-menu-item">
+          Reset Filters
+        </div>
       </div>
-    </nav>
+    </>
   );
 };
+
 export default Navbar;
