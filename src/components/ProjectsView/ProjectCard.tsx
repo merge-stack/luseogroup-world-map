8f4e33d15b7abc61595bc187126c9b478e75d7aa
Zainab Ilyas <zainab.ilyas@mergestack.com>
2025-03-27 13:14:38 +0500

    Improv: Add new project card design

--- src/components/ProjectsView/ProjectCard.tsx
+++ src/components/ProjectsView/ProjectCard.tsx
@@ -3,6 +3,15 @@ import { useCallback, useEffect } from "react";
 import { IProject } from "@interfaces";
 import { REACT_DEFAULT_IMAGE_URL } from "@config";
 
+import { startCase, toLower } from "lodash";
+import luseoPin from "@assets/images/luseo-pin.png";
+import architecte from "@assets/images/architecte.png";
+import surface from "@assets/images/surface.png";
+import bim from "@assets/images/bim.png";
+import certification from "@assets/images/certification.png";
+import mission from "@assets/images/mission.png";
+
+import { categoryIcons } from "../Map";
 import "./index.css";
 import ImageSlider from "./ImageSlider";
 
@@ -14,6 +23,22 @@ interface IProjectCard {
   scrollToProject: (projectId: number) => void
 }
 
+interface CardDetailPortionProps {
+  icon: string;
+  title: string;
+  value: string | null | undefined;
+}
+
+const CardDetailPortion: React.FC<CardDetailPortionProps> = ({ icon, title, value }) => (
+  <div className="card-detail-portion">
+    <img src={icon} alt={title} className="card-icons" />
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <span className="detail-title">{title}</span>
+      <span className="detail-value">{value || "N/A"}</span>
+    </div>
+  </div>
+);
+
 const ProjectCard: React.FC<IProjectCard> = ({
   project,
   selectedProject,
@@ -21,6 +46,8 @@ const ProjectCard: React.FC<IProjectCard> = ({
   projectRefs,
   scrollToProject
 }) => {
+  const categoryIcon = categoryIcons[project.category]
+
   const setProjectRef = useCallback(
     (el: HTMLDivElement | null) => {
       if (el) {
@@ -44,30 +71,16 @@ const ProjectCard: React.FC<IProjectCard> = ({
         <ImageSlider photos={project.photos?.length > 0 ? project.photos : [REACT_DEFAULT_IMAGE_URL]} photoHeight="180px" />
       </div>
       <div className="location-card-content" >
-        <h3 className="project-name">{project.name}</h3>
-        <span className="project-region">{`${project.city}, ${project.region}`}</span>
+        <h5 className="project-name">{project.name}</h5>
         <hr className="project-divider" />
         <div className="location-card-section">
-          <h3 className="location-card-subtitle">MISSION</h3>
-          <p className="location-card-text">{project.mission || "N/A"}</p>
-        </div>
-        <div className="location-card-section">
-          <h3 className="location-card-subtitle">DÈTAILS</h3>
-          <p className="location-card-text">
-            <h5 className="location-card-text-detail">TYPE DÈ PROJET:</h5> {project.category || "N/A"}
-          </p>
-          <p className="location-card-text">
-            <h5 className="location-card-text-detail">SURFACE:</h5> {project.area || "N/A"}
-          </p>
-          <p className="location-card-text">
-            <h5 className="location-card-text-detail">BIM:</h5> {project.bim || "N/A"}
-          </p>
-          <p className="location-card-text">
-            <h5 className="location-card-text-detail">CERTIFICATION:</h5> {project.certification || "N/A"}
-          </p>
-          <p className="location-card-text">
-            <h5 className="location-card-text-detail">ARCHITECTÈ: </h5> {project.architect || "N/A"}
-          </p>
+          <CardDetailPortion icon={luseoPin} title="Localisation" value={`${startCase(toLower(project.city))}, ${startCase(toLower(project.region))}`} />
+          <CardDetailPortion icon={categoryIcon || REACT_DEFAULT_IMAGE_URL} title="Type de Projet" value={project.category} />
+          <CardDetailPortion icon={architecte} title="Architecte" value={project.architect} />
+          <CardDetailPortion icon={surface} title="Surface" value={project.area} />
+          <CardDetailPortion icon={bim} title="BIM" value={project.bim} />
+          <CardDetailPortion icon={certification} title="Certification" value={project.certification} />
+          <CardDetailPortion icon={mission} title="Mission" value={project.mission} />
         </div>
       </div>
     </div>
